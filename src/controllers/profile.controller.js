af5cf01dc70d964d06b05c20e5e4c30a512e0c20
unknown <ananioluwatobiloba2000@gmail.com>
2020-12-20 20:30:16 +0100

    created accept and join org controllers

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -62,6 +62,80 @@ class ProfileController {
       });
     }
   }
+
+  /**
+   *  @description   A a user
+   *  @param { object }
+   *
+   *  @returns { object }
+   * */
+  static async AcceptInviteToJoinOrg(req, res) {
+    const { id: userId, email } = req.user;
+    const { jobRole, department } = req.body;
+
+    const { invitekey } = req.query;
+    const { orgId: organizationId } = req.params;
+
+    try {
+      if (!userId || !email || !invitekey || !organizationId) {
+        return httpResponse.error(res, 400, 'all fields required', true);
+      }
+      const checkIfUserExist = await pool.query('SELECT * FROM users WHERE id=$1', [userId]);
+      if (checkIfUserExist.rows[0]) {
+        const checkIfUserIsAmember = await pool.query('SELECT * FROM organizationMembers WHERE email=$1 AND invite_key=$2 AND organization_id=$3', [email, invitekey, organizationId]);
+        if (checkIfUserIsAmember.rows[0] && checkIfUserIsAmember.rows[0].has_joined === false && checkIfUserIsAmember.rows[0].email === email) {
+          const insertUserToOrg = await pool.query('INSERT INTO organizationMembers (email, user_id, has_joined, organization_id, jobRole, department) VALUES($1, $2, $3, $4) RETURNING *', [email, userId, true, organizationId, jobRole, department]);
+          if (insertUserToOrg.rows[0]) {
+            return httpResponse.success(res, 200, 'user Joined successfully', insertUserToOrg.rows[0]);
+          }
+          return httpResponse.error(res, 500, 'task failed', true);
+        }
+        return httpResponse.error(res, 400, 'user is already in the organization or invalid user', true);
+      }
+      return httpResponse.error(res, 400, 'users does not exist', true);
+    } catch (error) {
+      return res.status(500).json({
+        message: ` Error from server ${error}`,
+      });
+    }
+  }
+
+  /**
+   *  @description   A a user
+   *  @param { object }
+   *
+   *  @returns { object }
+   * */
+  static async JoinOpenOrg(req, res) {
+    const { id: userId, email } = req.user;
+    const { jobRole, department } = req.body;
+    const { orgId: organizationId } = req.params;
+
+    try {
+      if (!userId || !email || !organizationId) {
+        return httpResponse.error(res, 400, 'all fields required', true);
+      }
+      const checkIfUserExist = await pool.query('SELECT * FROM users WHERE id=$1', [userId]);
+      if (checkIfUserExist.rows[0]) {
+        const checkIfUserIsAmember = await pool.query('SELECT * FROM organizationMembers WHERE email=$1 AND organization_id=$2', [email, organizationId]);
+        if (!checkIfUserIsAmember.rows[0]) {
+          const checkIfOrgIsPublic = await pool.query('SELECT * FROM organization WHERE organization_id=$1', [organizationId]);
+          // eslint-disable-next-line eqeqeq
+          if (checkIfOrgIsPublic.rows[0].isprivate == false) {
+            const insertUserToOrg = await pool.query('INSERT INTO organizationMembers (email, user_id, has_joined, organization_id, jobRole, department) VALUES($1, $2, $3, $4) RETURNING *', [email, userId, true, organizationId, jobRole, department]);
+            return httpResponse.success(res, 200, 'user Joined successfully', insertUserToOrg.rows[0]);
+          }
+          return httpResponse.error(res, 500, 'org is private', true);
+        }
+        return httpResponse.error(res, 400, 'user is already in the organization or invalid user', true);
+      }
+      return httpResponse.error(res, 400, 'users does not exist', true);
+    } catch (error) {
+      return res.status(500).json({
+        message: ` Error from server ${error}`,
+      });
+    }
+  }
 }
 
 export default ProfileController;
