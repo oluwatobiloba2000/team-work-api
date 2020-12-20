06735948dac0d41696555868c15868669cffee75
unknown <ananioluwatobiloba2000@gmail.com>
2020-12-20 20:43:12 +0100

    added route to flag a comment

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -8,4 +8,5 @@ const router = Router();
 router.post('/comment/:postId/create', checkToken, Comment.CreateComment);
 router.put('/comment/:commentId/delete', checkToken, Comment.deleteComment);
 router.get('/comment/:postId/all', checkToken, Comment.commentFeed);
+router.post('/comment/:commentId/flag', checkToken, Comment.flagCommentAsInAppropriate);
 export default router;
