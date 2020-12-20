6b469bae04ea0ad3b3d51c7320f7811d7cf3d234
unknown <ananioluwatobiloba2000@gmail.com>
2020-12-20 20:50:02 +0100

    added route to display details of a org

--- src/routes/org.route.js
+++ src/routes/org.route.js
@@ -6,6 +6,7 @@ import checkToken from '../middleware/checkToken';
 const router = Router();
 
 router.post('/org/create', checkToken, Organization.Create);
+router.get('/org/:orgId/details', checkToken, Organization.orgDetails);
 router.get('/user/org', checkToken, Organization.AllUserOrg);
 router.put('/org/:orgId/admin/:userId/add', checkToken, Organization.AssignAdmin);
 
@@ -13,4 +14,5 @@ router.put('/org/:orgId/admin/:userId/add', checkToken, Organization.AssignAdmin
 router.put('/org/:orgId/admin/:userId/dismiss', checkToken, Organization.DismissAdmin);
 
 router.put('/admin/org/:orgId/privacy', checkToken, Organization.Privacy);
+router.get('/org/public/all', checkToken, Organization.FetchAllPublicOrgs);
 export default router;
