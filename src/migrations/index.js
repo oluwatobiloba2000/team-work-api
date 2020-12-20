d9964b677ac81c15e428311b00918e097badb9b9
unknown <ananioluwatobiloba2000@gmail.com>
2020-12-20 20:39:06 +0100

    added condition to check if migrate fun returns true

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-console */
-import migrate from './migration';
+import { migrate } from './migration';
 
 import db from '../db/index';
 
 (async () => {
   try {
-    await migrate(db);
-    console.log('database migrated successfully');
+    // eslint-disable-next-line max-len
+    const result = await migrate(db);
+    result && console.log('database migrated successfully');
     process.exit();
   } catch (error) {
     console.log(error);
