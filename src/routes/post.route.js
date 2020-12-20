92e7bf5be12c319b40ff4e4777d1ec4feedbe857
unknown <ananioluwatobiloba2000@gmail.com>
2020-12-20 20:56:23 +0100

    added more post routes

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -5,9 +5,11 @@ import checkToken from '../middleware/checkToken';
 
 const router = Router();
 
-router.post('/post/:orgId', checkToken, Post.CreatePost);
+router.post('/post/:orgId/create', checkToken, Post.CreatePost);
 router.put('/post/:postId/edit', checkToken, Post.Update);
 router.delete('/post/:postId/delete', checkToken, Post.deletePost);
 router.get('/org/:orgId/feed', checkToken, Post.postFeed);
 router.get('/post/:postId', checkToken, Post.singlePost);
+router.post('/post/:postId/flag', checkToken, Post.flagPostAsInAppropriate);
+router.get('/org/:orgId/tag/feed', checkToken, Post.postByTagFeed);
 export default router;
