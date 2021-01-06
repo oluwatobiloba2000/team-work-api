02f4d2222574779a6285842f408947795e3bc96f
unknown <ananioluwatobiloba2000@gmail.com>
2021-01-06 16:27:47 +0100

    removed cloudinary config

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,12 +1,6 @@
 import cloudinary from 'cloudinary';
 import 'dotenv/config';
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
 /** ******
  * @class Upload
  *
